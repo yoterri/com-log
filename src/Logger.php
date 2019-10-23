577a7966b398fdfbcbf4c5266a18dfaf42886c85
Yassir Terri Mejia <yoterri@ideasti.com>
2019-10-23 11:55:50 -0400

    fix

--- src/Logger.php
+++ src/Logger.php
@@ -42,7 +42,8 @@ class Logger extends zLogger
             }
         }
 
-        $extra = ['debug_value' => $extra];
+        $encoded = $this->_normalize($extra);
+        $extra = ['debug_value' => $encoded];
         $backtrace = debug_backtrace();
 
         #
@@ -212,5 +213,51 @@ class Logger extends zLogger
     public function warning($message, $extra = [], $ref = null)
     {
         return $this->log(self::WARN, $message, $extra, $ref);
+    }
+
+
+
+    /**
+     * Normalize all non-scalar data types (except null) in a string value
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    protected function _normalize($value)
+    {
+        if (is_scalar($value) || null === $value) {
+            return $value;
+        }
+
+        // better readable JSON
+        static $jsonFlags;
+        if ($jsonFlags === null) {
+            $jsonFlags = 0;
+            $jsonFlags |= defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0;
+            $jsonFlags |= defined('JSON_UNESCAPED_UNICODE') ? JSON_UNESCAPED_UNICODE : 0;
+            #$jsonFlags |= defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0;
+            
+        }
+
+        // Error suppression is used in several of these cases as a fix for each of
+        // #5383 and #4616. Without it, #4616 fails whenever recursion occurs during
+        // json_encode() operations; usage of a dedicated error handler callback
+        // causes #5383 to fail when the Logger is being used as an error handler.
+        // The only viable solution here is error suppression, ugly as it may be.
+        if ($value instanceof \DateTime) {
+            $value = $value->format($this->getDateTimeFormat());
+        } elseif ($value instanceof \Traversable) {
+            $value = @json_encode(iterator_to_array($value), $jsonFlags);
+        } elseif (is_array($value)) {
+            $value = @json_encode($value, $jsonFlags);
+        } elseif (is_object($value) && ! method_exists($value, '__toString')) {
+            $value = sprintf('object(%s) %s', get_class($value), @json_encode($value));
+        } elseif (is_resource($value)) {
+            $value = sprintf('resource(%s)', get_resource_type($value));
+        } elseif (! is_object($value)) {
+            $value = gettype($value);
+        }
+
+        return (string) $value;
     }
 }
